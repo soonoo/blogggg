0a43984f58fd68fe70d48bfee96ba360b5399b5d
soonoo <qpseh2m7@gmail.com>
2018-02-02 01:39:43 +0900

    edit 페이지 unescape 제거

--- pages/edit.js
+++ pages/edit.js
@@ -10,7 +10,7 @@ const Index = ({ json }) => (
 Index.getInitialProps = async ({ asPath }) => {
   const url = process.env.BACKEND_URL;
   const data = await fetch(`${url}/api/post/list`);
-  const json = await escape(data).json();
+  const json = await data.json();
   return {
     json
   };
