93716b69106ad904c4a5d65a75485d1c5391bf27
soonoo <qpseh2m7@gmail.com>
2018-05-06 02:48:00 +0900

    change. left padding of tag description

--- components/TagPageTitle.js
+++ components/TagPageTitle.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const TagPageTitle = ({tagName}) => (
-    <div className="tag_description">{tagName} 태그가 포함된 글 목록</div>
+    <div className="tag_description">
+        {tagName} 태그가 포함된 글 목록
+        <style>{`
+        .tag_description {
+            padding-left: 10px;
+        }
+        `}</style>
+    </div>
 );
 
 export default TagPageTitle;
