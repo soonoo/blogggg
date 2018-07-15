4a2cb1390a5541b8568b2d22f6d4ec17a66be74f
soonoo27 <soonoo@ad4th.com>
2018-07-15 20:19:58 +0900

    remove univ-cource menu

--- components/BlogTopic.js
+++ components/BlogTopic.js
@@ -23,7 +23,6 @@ const BlogTopic = () => {
       <ul className='title_bar'>
         <li><Link href='/'><a href='/'>Home</a></Link></li>
         <li><Link href='/about'><a>About</a></Link></li>
-        <li><Link href='/univ'><a>Univ-courses</a></Link></li>
         <li><Link href='/point'><a>리빙 포인트</a></Link></li>
         {/* <Link href='/write'><li><FaPencilSquare color='black' size={22} /></li></Link> */}
       </ul>
