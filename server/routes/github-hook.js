fd39c0f1c4b7a21f8b32bd2cc2954744b5cf3ea1
soonoo <qpseh2m7@gmail.com>
2019-01-26 10:11:36 +0000

    add `stdio` option in `exec` command

--- server/routes/github-hook.js
+++ server/routes/github-hook.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 const router = express.Router();
 router.use(bodyParser.json({ limit: '5mb' }));
 
-// 글 목록 가져오기
 router.post('/', async (req, res) => {
   if(req.body.ref === 'refs/heads/master') {
     res.sendStatus(200);
 
     // deploy-blog.sh should be in $PATH
-    exec('deploy-blog');
+    spawn('deploy-blog', [], {
+      //detached: true,
+      stdio: 'inherit',
+    });
   }
   else {
     res.sendStatus(202);
